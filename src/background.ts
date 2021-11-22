d96c835a26a5c12700d9d4a8500190f4614841b3
Vince Broz <vince@broz.cc>
2021-11-22 13:38:46 -0500

    Update template

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,9 @@
+/**
+ * background module.
+ *
+ * Initialized when browser extension is loaded.
+ */
+
 import { doWork } from './shortcuts_for_asana';
 
 // Called when the user clicks on the browser action.
--- src/shortcuts_for_asana.ts
+++ src/shortcuts_for_asana.ts
@@ -1,3 +1,9 @@
+/**
+ * shortcuts_for_asana module.
+ *
+ * Chrome extension which adds missing keyboard shortcuts/behavior to Asana
+ */
+
 export const doWork = (tab: chrome.tabs.Tab) => {
   // No tabs or host permissions needed!
   console.log(`Turning ${tab.url} red!`);
