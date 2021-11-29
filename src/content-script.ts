1a657ac6995edc0cc2ffee4217795f0f0734c90a
Vince Broz <vince@broz.cc>
2021-11-29 13:56:07 -0500

    Refactor behaviors into named functions

--- src/content-script.ts
+++ src/content-script.ts
@@ -30,41 +30,83 @@ const dependencyLinks = (): HTMLElement[] => {
 // .ProsemirrorEditor-link is a link to an outside site
 const bodyLinks = () => Array.from(document.querySelectorAll('.PrimaryNavigationLink,.ProsemirrorEditor-link'));
 
+const focusOnFirstTask = () => {
+  console.log('trying to focus on first task');
+  const firstTextArea = document.querySelector('textarea.SpreadsheetTaskName-input');
+  if (firstTextArea == null || !(firstTextArea instanceof HTMLElement)) {
+    logError('Invalid text area');
+  }
+  console.log('first text area', firstTextArea);
+
+  // don't switch task if a subtask was marked done
+  firstTextArea.click();
+};
+
+const removeAssignee = () => {
+  console.log('trying to remove assignee');
+  const element = document.querySelector('div.RemoveButton');
+  if (element != null && element instanceof HTMLElement) {
+    element.click();
+  }
+};
+
+const markTaskWithIncompleteDependentsDialogComplete = () => {
+  console.log('got meta enter (before others)');
+  const markCompleteSelector = 'div.CompleteTaskWithIncompletePrecedentTasksConfirmationModal div.PrimaryButton';
+  const element = document.querySelector(markCompleteSelector);
+  if (element != null && element instanceof HTMLElement) {
+    element.click();
+  }
+};
+
+const openLink = (num: number) => {
+  const dependencies = dependencyLinks();
+  if (dependencies.length > 0) {
+    const linkFound = dependencies[num - 1];
+    console.log('linkFound', linkFound);
+    if (linkFound != null) {
+      linkFound.click();
+    }
+  } else {
+    const links = bodyLinks();
+    const linkFound = links[num - 1];
+    console.log('linkFound', linkFound);
+    if (linkFound != null) {
+      const url = linkFound.getAttribute('href');
+      if (url != null) {
+        window.open(url, '_blank');
+      }
+    }
+  }
+};
+
 console.log('Defining shortcutsKeyDownBeforeOthers');
 const shortcutsKeyDownBeforeOthers = (e: KeyboardEvent) => {
   // this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
   const nonZeroDigits = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
   if (e.metaKey && e.ctrlKey && nonZeroDigits.includes(e.key)) {
     const num = parseInt(e.key, 10);
-    const dependencies = dependencyLinks();
-    if (dependencies.length > 0) {
-      const linkFound = dependencies[num - 1];
-      console.log('linkFound', linkFound);
-      if (linkFound != null) {
-        linkFound.click();
-      }
-    } else {
-      const links = bodyLinks();
-      const linkFound = links[num - 1];
-      console.log('linkFound', linkFound);
-      if (linkFound != null) {
-        const url = linkFound.getAttribute('href');
-        if (url != null) {
-          window.open(url, '_blank');
-        }
-      }
-    }
+    openLink(num);
   } else if (e.metaKey && e.ctrlKey && e.key === 'r') {
-    const element = document.querySelector('div.RemoveButton');
-    if (element != null && element instanceof HTMLElement) {
-      element.click();
-    }
+    removeAssignee();
   } else if (e.metaKey && e.key === 'Enter') {
-    console.log('got meta enter (before others)');
-    const markCompleteSelector = 'div.CompleteTaskWithIncompletePrecedentTasksConfirmationModal div.PrimaryButton';
-    const element = document.querySelector(markCompleteSelector);
-    if (element != null && element instanceof HTMLElement) {
-      element.click();
+    markTaskWithIncompleteDependentsDialogComplete();
+  }
+};
+
+const markTaskComplete = () => {
+  console.log('got meta enter (after others)');
+  const element = document.querySelector('div.CompleteTaskWithIncompletePrecedentTasksConfirmationModal');
+  if (element == null) {
+    // don't switch task if the cmd-enter key created a modal
+    const classes = document.activeElement?.parentElement?.classList;
+    if (classes == null) {
+      logError('Could not find element');
+    }
+    const isOnSubtask = classes.contains('SubtaskTaskRow-taskName');
+    console.log({ isOnSubtask });
+    if (!isOnSubtask) {
+      focusOnFirstTask();
     }
   }
 };
@@ -72,26 +114,7 @@ const shortcutsKeyDownBeforeOthers = (e: KeyboardEvent) => {
 console.log('Defining shortcutsKeyDownAfterOthers');
 const shortcutsKeyDownAfterOthers = (e: KeyboardEvent) => {
   if (e.metaKey && e.key === 'Enter') {
-    console.log('got meta enter (after others)');
-    const firstTextArea = document.querySelector('textarea.SpreadsheetTaskName-input');
-    if (firstTextArea == null || !(firstTextArea instanceof HTMLElement)) {
-      logError('Invalid text area');
-    }
-    console.log('first text area', firstTextArea);
-    const element = document.querySelector('div.CompleteTaskWithIncompletePrecedentTasksConfirmationModal');
-    if (element == null) {
-      // don't switch task if the cmd-enter key created a modal
-      const classes = document.activeElement?.parentElement?.classList;
-      if (classes == null) {
-        logError('Could not find element');
-      }
-      const isOnSubtask = classes.contains('SubtaskTaskRow-taskName');
-      console.log({ isOnSubtask });
-      if (!isOnSubtask) {
-        // don't switch task if a subtask was marked done
-        firstTextArea.click();
-      }
-    }
+    markTaskComplete();
   }
 };
 
