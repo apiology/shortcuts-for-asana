a3aa061f1113ea5ca4a31021a873d2d2eb3bca4c
Vince Broz <vince@broz.cc>
2023-03-02 19:41:32 -0500

    Test branch

--- src/shortcuts-for-asana.test.ts
+++ src/shortcuts-for-asana.test.ts
@@ -62,3 +62,11 @@ test('setting task time dismisses task time', () => {
   shortcutsKeyDownBeforeOthers(new window.KeyboardEvent('keydown', { ctrlKey: true, metaKey: false, key: 't' }));
   expect(dismissTaskTimeButtonParent.click).toHaveBeenCalledWith();
 });
+
+test('setting task time with no existing time', () => {
+  // this is counterfactual - as of 2023-03 there's actually a button
+  // there regardless, even if it's not visible
+  document.body.innerHTML = '<html><div class="TaskDueDateToken"><div id="task_pane_due_date_input" class="ThemeableCardPresentation--isValid ThemeableCardPresentation ThemeableInteractiveCardPresentation--isNotSelected ThemeableInteractiveCardPresentation--isEnabled ThemeableInteractiveCardPresentation SubtleButtonCard SubtleTokenButton--empty SubtleTokenButton--editable SubtleTokenButton--subtle SubtleTokenButton DueDateTokenButton--empty DueDateTokenButton--editable DueDateTokenButton--future DueDateTokenButton TaskDueDateToken-tokenButton TaskDueDateToken--future" role="button" tabindex="0" aria-disabled="false" aria-describedby="task_pane_due_date_label" aria-expanded="false"><div class="SubtleTokenButton-leftIcon"><svg class="DashedIcon--small DashedIcon DueDateTokenButton-emptyIcon DueDateDashedIcon" focusable="false" viewBox="0 0 28 28"><path d="M27.5,14c0,7.5-6,13.5-13.5,13.5S0.5,21.5,0.5,14c0-4.6,2.3-8.7,5.9-11.2c2.2-1.5,4.8-2.3,7.6-2.3 C21.5,0.5,27.5,6.5,27.5,14z" class="DashedIcon-outer"></path><path d="M18,7V6.5C18,6.2,17.8,6,17.5,6S17,6.2,17,6.5V7h-6V6.5C11,6.2,10.8,6,10.5,6S10,6.2,10,6.5V7c-1.7,0-3,1.3-3,3v8 c0,1.7,1.3,3,3,3h8c1.7,0,3-1.3,3-3v-8C21,8.3,19.7,7,18,7z M10,8v0.5C10,8.8,10.2,9,10.5,9S11,8.8,11,8.5V8h6v0.5 C17,8.8,17.2,9,17.5,9S18,8.8,18,8.5V8c1.1,0,2,0.9,2,2v1H8v-1C8,8.9,8.9,8,10,8z M18,20h-8c-1.1,0-2-0.9-2-2v-6h12v6 C20,19.1,19.1,20,18,20z" class="DashedIcon-inner"></path></svg></div><span class="DueDateTokenButton-label Typography Typography--overflowTruncate">No due date</span></div></div></html>';
+  setPlatform(new TestPlatform());
+  shortcutsKeyDownBeforeOthers(new window.KeyboardEvent('keydown', { ctrlKey: true, metaKey: false, key: 't' }));
+});
